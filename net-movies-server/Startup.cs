db0979f9c3fe3f51e1e5ec9b602889143e082720
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-09-07 20:21:05 +0300

    as

--- net-movies-server/Startup.cs
+++ net-movies-server/Startup.cs
@@ -43,9 +43,6 @@ namespace SearchMovieServer
             //services.AddScoped<AccountService>();
             services.AddScoped<MovieService>();
             services.AddControllersWithViews();
-            //.AddNewtonsoftJson(options =>
-            // options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            //);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
